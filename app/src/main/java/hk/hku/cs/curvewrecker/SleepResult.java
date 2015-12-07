9020a510ab9dcd71c534c536ae0ca529aafa3faf
vannahz <waner0120@gmail.com>
2015-12-07 19:40:03 +0800

    睡觉模块已添

---
+++ app/src/main/java/hk/hku/cs/curvewrecker/SleepResult.java
@@ -0,0 +1,40 @@
+package hk.hku.cs.curvewrecker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by vannahz on 2015/12/6.
+ */
+public class SleepResult extends AppCompatActivity {
+
+    private Button fight_btn;
+    private Toolbar toolbar;
+    private TextView toolbar_title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sleep_result);
+        fight_btn = (Button) findViewById(R.id.fight_btn);
+
+        toolbar_title = (TextView)findViewById(R.id.toolbar_title);
+        toolbar_title.setText("Morning");
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        fight_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/hk/hku/cs/curvewrecker/SleepSetting.java
@@ -0,0 +1,65 @@
+package hk.hku.cs.curvewrecker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by vannahz on 2015/12/6.
+ */
+public class SleepSetting extends AppCompatActivity {
+
+
+    private TextView toolbar_title;
+    private Toolbar toolbar;
+    private Button sleepnow_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sleep_setting);
+
+        //设置Toolbar
+        toolbar_title = (TextView)findViewById(R.id.toolbar_title);
+        toolbar_title.setText("Time Setting");
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+        //start button
+        sleepnow_btn = (Button)findViewById(R.id.sleepnow_btn);
+        sleepnow_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSleeping();
+            }
+        });
+    }
+
+    public void goToSleeping() {
+
+        setContentView(R.layout.sleeping);
+
+        toolbar_title = (TextView)findViewById(R.id.toolbar_title);
+        toolbar_title.setText("Sleeping");
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+        Button wakeup_btn = (Button)findViewById(R.id.wakeup_btn);
+        wakeup_btn.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SleepSetting.this, SleepResult.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+
+}
