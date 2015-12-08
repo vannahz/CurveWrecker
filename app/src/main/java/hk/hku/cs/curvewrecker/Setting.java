ab54aac0a40e0cde440924490121e65689c0d428
ycraaron <ycr_aaron@foxmail.com>
2015-12-08 21:46:44 +0800

    BUG fixed

--- app/src/main/java/hk/hku/cs/curvewrecker/Setting.java
+++ app/src/main/java/hk/hku/cs/curvewrecker/Setting.java
@@ -1,14 +1,9 @@
 package hk.hku.cs.curvewrecker;
 
-import android.app.LauncherActivity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-public class Setting extends AppCompatActivity {
+public class Setting extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
