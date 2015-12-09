0d91f2214b978a87dff1b162e060aa2d44e7a858
vannahz <waner0120@gmail.com>
2015-12-10 00:29:38 +0800

    滚轮式时间设定添加完毕

--- app/src/main/java/hk/hku/cs/curvewrecker/StudySetting.java
+++ app/src/main/java/hk/hku/cs/curvewrecker/StudySetting.java
@@ -1,13 +1,14 @@
 package hk.hku.cs.curvewrecker;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by vannahz on 2015/12/3.
@@ -17,6 +18,8 @@ public class StudySetting extends AppCompatActivity {
     private TextView toolbar_title;
     private Toolbar toolbar;
     private Button start_btn;
+    private LinearLayout studySetting;
+    private TextView study_hour, study_minute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +31,32 @@ public class StudySetting extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+
+        study_hour = (TextView)findViewById(R.id.study_hour);
+        study_minute = (TextView)findViewById(R.id.study_minute);
+
+        //change time
+        studySetting = (LinearLayout)findViewById(R.id.studySetting);
+        studySetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangeBirthDialog mChangeBirthDialog = new ChangeBirthDialog(StudySetting.this);
+                mChangeBirthDialog.setDate(1, 0);
+                mChangeBirthDialog.show();
+                mChangeBirthDialog.setTimeListener(new ChangeBirthDialog.OnTimeListener() {
+                    @Override
+                    public void onClick(String hour, String minute) {
+                        Toast.makeText(StudySetting.this,hour + ":" + minute,Toast.LENGTH_LONG).show();
+                        study_hour.setText("0" + hour);
+
+                        if(Integer.parseInt(minute) < 10)
+                            minute = "0" + minute;
+                        study_minute.setText(minute);
+                    }
+                });
+            }
+        });
+
 
         //start button
         start_btn = (Button)findViewById(R.id.start_btn);
